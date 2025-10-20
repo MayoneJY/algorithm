cf6726012bf66fa4453cc79822adc93b031088ad
MayoneJY <32291392+MayoneJY@users.noreply.github.com>
2025-10-20 16:21:38 +0900

    [Gold II] Title: 골목 대장 호석 - 효율성 2, Time: 1140 ms, Memory: 269884 KB, Score: 33 point -BaekjoonHub

--- 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.java
+++ 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.java
@@ -3,15 +3,17 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, S, E, max;
+    static int N, M, S, E;
+    static long max;
     static Map<Integer, List<Edge>> edges = new HashMap<>();
     static class Edge{
-        int v, w, m = 0;
-        Edge(int v, int w){
+        int v;
+        long w, m = 0;
+        Edge(int v, long w){
             this.v = v;
             this.w = w;
         }
-        Edge(int v, int w, int m){
+        Edge(int v, long w, long m){
             this.v = v;
             this.w = w;
             this.m = m;
@@ -20,22 +22,17 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        try{
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-            S = Integer.parseInt(st.nextToken());
-            E = Integer.parseInt(st.nextToken());
-            max = Integer.parseInt(st.nextToken());
-        }
-        catch (NumberFormatException e){
-            return;
-        }
-
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        max = Long.parseLong(st.nextToken());
+        
         for(int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
+            long v = Long.parseLong(st.nextToken());
             edges.putIfAbsent(s, new ArrayList<>());
             edges.putIfAbsent(e, new ArrayList<>());
 
@@ -48,30 +45,34 @@ public class Main {
 
     static void bfs(){
         PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2) -> {
-            return o1.w-o2.w;
+            if(o1.m < o2.m)
+                return Long.compare(o1.m, o2.m);
+            return Long.compare(o1.w, o2.w);
         });
 
-        int[] visited = new int[N+1];
-        Arrays.fill(visited, Integer.MAX_VALUE);
+        long[] visited = new long[N+1];
+        Arrays.fill(visited, Long.MAX_VALUE);
         
         visited[S] = 0;
         pq.add(new Edge(S, 0));
-        int result = Integer.MAX_VALUE;
         while (!pq.isEmpty()) {
             Edge now = pq.poll();
-            if(now.v == E)
-                result = Math.min(result, now.m);
+
+            if(now.v == E){
+                System.out.println(now.m);
+                return;
+            }
+
+            if(visited[now.v] < now.w) continue;
+
             for(Edge e: edges.get(now.v)){
                 if(visited[now.v] + e.w <= max && visited[e.v] > visited[now.v] + e.w){
-                    int mm = Math.max(now.m, e.w);
+                    long mm = Math.max(now.m, e.w);
                     visited[e.v] = visited[now.v] + e.w;
-                    pq.add(new Edge(e.v, e.w, mm));
+                    pq.add(new Edge(e.v, visited[e.v], mm));
                 }
             }
         }
-        if(result == Integer.MAX_VALUE)
-            System.out.println(-1);
-        else
-            System.out.println(result);
+        System.out.println(-1);
     }
 }
